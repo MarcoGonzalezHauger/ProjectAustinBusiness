47ad304e16f0a411b8fbff36e6d1472fbf585945
MarcoGonzalezHauger <marcogonzalezhauger@gmail.com>
2019-05-08 13:40:54 -0400

    ADDED some money tab ui

---
+++ Ambassadoor Business/Money/MoneyVC.swift
@@ -0,0 +1,23 @@
+//
+//  MoneyVC.swift
+//  Ambassadoor Business
+//
+//  Created by Marco Gonzalez Hauger on 5/8/19.
+//  Copyright © 2019 Tesseract Freelance, LLC. All rights reserved.
+//	Exclusive Property of Tesseract Freelance, LLC.
+//
+
+import UIKit
+
+class MoneyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		
+    }
+
+}
