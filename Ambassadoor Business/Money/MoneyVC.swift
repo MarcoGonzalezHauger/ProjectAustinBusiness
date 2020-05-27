171d5c693e8ff01abfb5d384bcbc7aa507d63598
MarcoGonzalezHauger <marcogonzalezhauger@gmail.com>
2020-05-26 22:43:03 -0400

    made money tab changes

--- Ambassadoor Business/Money/MoneyVC.swift
+++ Ambassadoor Business/Money/MoneyVC.swift
@@ -12,100 +12,56 @@ import Firebase
 import FirebaseCore
 import FirebaseInstanceID
 
-enum cellAction {
-	case deposit, withdraw
-}
-
-protocol cellDelegate {
-	func actionSent(action: cellAction)
-}
-
-struct Transaction {
-	let description: String
-	let details: AnyObject
-	let time: String
-	let amount: Double
-    let type: String
-    let status: String
-    let userName: String
-}
-
-class BalanceCell: UITableViewCell {
-	var delegate: cellDelegate?
-	@IBOutlet weak var balanceLabel: UILabel!
-	@IBAction func deposit(_ sender: Any) {
-		delegate?.actionSent(action: .deposit)
-	}
-	@IBAction func withdraw(_ sender: Any) {
-		delegate?.actionSent(action: .withdraw)
-	}
-}
-
 class TransactionCell: UITableViewCell {
 	@IBOutlet weak var descriptionLabel: UILabel!
 	@IBOutlet weak var amountlabel: UILabel!
 	@IBOutlet weak var shadowBox: ShadowView!
 }
 
-class MoneyVC: UIViewController, UITableViewDelegate, UITableViewDataSource, TransactionListener, cellDelegate {
+class MoneyVC: UIViewController, UITableViewDelegate, UITableViewDataSource, TransactionListener {
 	
-	func actionSent(action: cellAction) {
-		if action == .deposit {
-			//depositVC must appear.
-		} else if action == .withdraw {
-			//withdraw VC must appear.
-		}
-	}
+	@IBOutlet weak var balanceLabel: UILabel!
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return transactionHistory.count + 1
+		return transactionHistory.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let row = indexPath.row
-		if row == 0 {
-			let cell = shelf.dequeueReusableCell(withIdentifier: "BalanceBox") as! BalanceCell
-			cell.balanceLabel.text = NumberToPrice(Value: accountBalance, enforceCents: true)
-			cell.delegate = self
-			return cell
-		} else {
-			let cell = shelf.dequeueReusableCell(withIdentifier: "TransactionTrunk") as! TransactionCell
-			let ThisTransaction = transactionHistory[row - 1]
-			var isNegative = false
-			let amt = NumberToPrice(Value: ThisTransaction.amount, enforceCents: true)
-            if ThisTransaction.type == "sale"{
-				cell.descriptionLabel.text = "Deposited \(amt) into Ambassadoor"
-				cell.amountlabel.text = NumberToPrice(Value: ThisTransaction.amount, enforceCents: true)
-            }else if ThisTransaction.type == "paid" {
-                cell.descriptionLabel.text = "Spent \(amt) to distribute \"\(ThisTransaction.status)\""
-				cell.amountlabel.text = "-\(NumberToPrice(Value: ThisTransaction.amount, enforceCents: true))"
-				isNegative = true
-            }else if ThisTransaction.type == "refund" {
-				cell.descriptionLabel.text = "User Rejected \"\(ThisTransaction.status)\", You have been credited \(amt)"
-				cell.amountlabel.text = NumberToPrice(Value: ThisTransaction.amount, enforceCents: true)
-            }else if ThisTransaction.type == "commissionrefund" {
-                cell.descriptionLabel.text = "Ambassadoor Commission Refunded \"\(ThisTransaction.status)\", You have been credited \(amt)"
-                cell.amountlabel.text = NumberToPrice(Value: ThisTransaction.amount, enforceCents: true)
-            }else if ThisTransaction.type == "postrefund" {
-                cell.descriptionLabel.text = "Ambassadoor Refunded the single post, You have been credited \(amt)"
-				cell.amountlabel.text = NumberToPrice(Value: ThisTransaction.amount, enforceCents: true)
-            }
-			cell.shadowBox.borderColor = isNegative ? .systemRed : .systemGreen
-			return cell
-			
+		let cell = shelf.dequeueReusableCell(withIdentifier: "TransactionTrunk") as! TransactionCell
+		let ThisTransaction = transactionHistory[row]
+		
+		
+		
+		
+		
+		let amt = NumberToPrice(Value: ThisTransaction.amount, enforceCents: true)
+		if ThisTransaction.type == "sale"{
+			cell.descriptionLabel.text = "Deposited \(amt) into Ambassadoor"
+			cell.amountlabel.text = NumberToPrice(Value: ThisTransaction.amount, enforceCents: true)
+		}else if ThisTransaction.type == "paid" {
+			cell.descriptionLabel.text = "Spent \(amt) to distribute \"\(ThisTransaction.status)\""
+			cell.amountlabel.text = "-\(NumberToPrice(Value: ThisTransaction.amount, enforceCents: true))"
+		}else if ThisTransaction.type == "refund" {
+			cell.descriptionLabel.text = "User Rejected \"\(ThisTransaction.status)\", You have been credited \(amt)"
+			cell.amountlabel.text = NumberToPrice(Value: ThisTransaction.amount, enforceCents: true)
+		}else if ThisTransaction.type == "commissionrefund" {
+			cell.descriptionLabel.text = "Ambassadoor Commission Refunded \"\(ThisTransaction.status)\", You have been credited \(amt)"
+			cell.amountlabel.text = NumberToPrice(Value: ThisTransaction.amount, enforceCents: true)
+		}else if ThisTransaction.type == "postrefund" {
+			cell.descriptionLabel.text = "Ambassadoor Refunded the single post, You have been credited \(amt)"
+			cell.amountlabel.text = NumberToPrice(Value: ThisTransaction.amount, enforceCents: true)
 		}
+		cell.shadowBox.borderColor = .black // Refer to PowerPoint.
+		return cell
 	}
 	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		let row = indexPath.row
-		if row > 0 {
-			
-		}
 		shelf.deselectRow(at: indexPath, animated: false)
 	}
 	
 	func BalanceChange() {
-		shelf.reloadRows(at: [IndexPath(row: 0, section: 0)], with: .none)
+		balanceLabel.text = NumberToPrice(Value: accountBalance, enforceCents: true)
 	}
 	
 	func TransactionHistoryChanged() {
@@ -115,15 +71,7 @@ class MoneyVC: UIViewController, UITableViewDelegate, UITableViewDataSource, Tra
 	@IBOutlet weak var shelf: UITableView!
 	
 	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-		let row = indexPath.row
-		if row == 0 {
-			return 230
-		}
-		if row == transactionHistory.count {
-			return 90
-		} else {
-			return 80
-		}
+		return 80
 	}
 	
 	override func viewDidLoad() {
@@ -134,38 +82,7 @@ class MoneyVC: UIViewController, UITableViewDelegate, UITableViewDataSource, Tra
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        
-//        if global.launchWay == "shortcut"{
-//
-//        //getCurrentCompanyUser(userID: (Auth.auth().currentUser?.uid)!) { (companyUser, error) in
-//            getDepositDetails(companyUser: (Auth.auth().currentUser?.uid)!) { (deposit, status, error) in
-//
-//                transactionHistory.removeAll()
-//
-//
-//                if status == "success" {
-//                    accountBalance = deposit!.currentBalance!
-//                    for value in deposit!.depositHistory! {
-//
-//                        if let valueDetails = value as? NSDictionary {
-//
-//                            transactionHistory.append(Transaction(description: "", details: valueDetails["cardDetails"] as AnyObject, time: valueDetails["updatedAt"] as! String, amount: Double(valueDetails["amount"] as! String)!, type: valueDetails["type"] as! String, status: valueDetails["status"] as? String ?? "", userName: valueDetails["userName"] as? String ?? ""))
-//                        }
-//                    }
-//                    //transactionDelegate = self
-//                    DispatchQueue.main.async(execute: {
-//                        self.shelf.delegate = self
-//                        self.shelf.dataSource = self
-//                        self.shelf.reloadData()
-//                    })
-//                }
-//
-//            }
-//
-//        }else{
-        
         self.getDeepositDetails()
-//        }
     }
 	
 	var shownBefore = false
