5894647181f29a9d43818a28903b863b0ae29e38
anushram <itramkumar.78@gmail.com>
2021-05-18 19:02:55 +0530

    Bug Fixes

---
+++ Ambassadoor Business/Company/ChangeWebsiteVC.swift
@@ -0,0 +1,104 @@
+//
+//  ChangeWebsiteVC.swift
+//  Ambassadoor Business
+//
+//  Created by Marco Gonzalez Hauger on 5/22/20.
+//  Copyright © 2020 Tesseract Freelance, LLC. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+protocol webChangedDelegate {
+	func websiteChanged(_ newWebsite: String)
+}
+
+class ChangeWebsiteVC: BaseVC {
+
+	var webChangedDelegate: webChangedDelegate?
+	@IBOutlet weak var webTextView: UITextField!
+	@IBOutlet weak var webView: WKWebView!
+	@IBOutlet weak var doneButton: UIButton!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		webTextView.text = toChangeTo
+		updateBack()
+		
+        // Do any additional setup after loading the view.
+    }
+	
+	var toChangeTo = ""
+	
+	func setDefaultUrl(_ url: String?) {
+		toChangeTo = url ?? ""
+		if toChangeTo.lowercased().hasPrefix("http://www.") {
+			toChangeTo = String(toChangeTo.dropFirst(11))
+		} else if toChangeTo.lowercased().hasPrefix("https://www.") {
+			toChangeTo = String(toChangeTo.dropFirst(12))
+		}
+	}
+	
+	@IBAction func dismissButton(_ sender: Any) {
+		dismiss(animated: true, completion: nil)
+	}
+	
+	@IBAction func saveButtonPressed(_ sender: Any) {
+        
+        let thhh = GetURL()!.absoluteString
+        
+        if !isGoodUrl(url: GetURL()!.absoluteString) || GetURL()!.absoluteString == "http://www.com" || GetURL()!.absoluteString == "https://www.com" || GetURL()!.absoluteString == "http://.com" || GetURL()!.absoluteString == "https://.com" || GetURL()!.absoluteString == "http://www..com" || GetURL()!.absoluteString == "https://www..com"{
+            
+           self.showAlertMessage(title: "Alert", message: "Please enter valid website") {
+                          
+           }
+            
+        }else{
+            
+            webChangedDelegate?.websiteChanged(GetURL()!.absoluteString)
+                       dismiss(animated: true, completion: nil)
+            
+            
+            
+        }
+	}
+	
+	@IBAction func donePressed(_ sender: Any) {
+		webTextView.resignFirstResponder()
+	}
+	var loadedPage: String = ""
+	
+	@IBAction func edited(_ sender: Any) {
+		updateBack()
+	}
+	
+	func updateBack() {
+		let thisUrl = GetURL()
+		doneButton.isHidden = thisUrl == nil
+        
+		if loadedPage != thisUrl?.absoluteString {
+			if let url = thisUrl {
+				webView.load(URLRequest(url: url))
+				loadedPage = url.absoluteString
+			}
+		}
+        
+        
+        
+	}
+	
+	func GetURL() -> URL? {
+		var returnValue = webTextView.text!
+		if !returnValue.lowercased().hasPrefix("http") {
+			if !returnValue.lowercased().hasPrefix("www.") {
+                
+				returnValue = "http://www." + returnValue
+            }
+            else{
+                returnValue = "http://" + returnValue
+            }
+		}
+		return URL.init(string: returnValue)
+	}
+}
--- Ambassadoor Business/Offer Editor/PostDetailVC.swift
+++ Ambassadoor Business/Offer Editor/PostDetailVC.swift
@@ -135,13 +135,20 @@ class KeyphraseCell: UITableViewCell, UITextFieldDelegate {
 }
 
 
-class PostDetailVC: BaseVC, UITableViewDelegate, UITableViewDataSource, deletePhrase {
-    
+class PostDetailVC: BaseVC, UITableViewDelegate, UITableViewDataSource, deletePhrase, NCDelegate {
+	
+	func shouldAllowBack() -> Bool {
+		return tryDismiss()
+	}
     
     func deleteThis(cell: UITableViewCell) {
         if phraseList.count <= 1 {
             if let newcell = (cell as? KeyphraseCell) {
                 newcell.phraseText.text = ""
+                if let index = self.shelf.indexPath(for: newcell){
+                    phraseList[index.row] = ""
+                }
+                self.shelf.reloadData()
                 MakeShake(viewToShake: cell, coefficient: 0.2)
             } else {
                 MakeShake(viewToShake: cell)
@@ -155,6 +162,7 @@ class PostDetailVC: BaseVC, UITableViewDelegate, UITableViewDataSource, deletePh
     
     func textChanged(at cell: UITableViewCell, to: String) {
         let ip: IndexPath = shelf.indexPath(for: cell)!
+        print("tick =",ip.row)
         phraseList[ip.row] = to
     }
     
@@ -181,6 +189,7 @@ class PostDetailVC: BaseVC, UITableViewDelegate, UITableViewDataSource, deletePh
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setPostDetails()
+		(self.navigationController as! StandardNC).tempDelegate = self
         // Do any additional setup after loading the view.
     }
     
@@ -202,11 +211,14 @@ class PostDetailVC: BaseVC, UITableViewDelegate, UITableViewDataSource, deletePh
                 let post = draftOffer?.draftPosts[postIndex!]
                 phraseList.append(contentsOf: post!.requiredKeywords)
                 for hashtag in post!.requiredHastags {
-                    if hashtag.starts(with: "#") {
-                        phraseList.append("\(hashtag)")
-                    }else{
-                        phraseList.append("#\(hashtag)")
-                    }
+					if hashtag != "ad" && hashtag != "#ad" {
+						if hashtag.starts(with: "#") {
+							phraseList.append("\(hashtag)")
+						}else{
+							phraseList.append("#\(hashtag)")
+						}
+					}
+                    
                 }
                 
                 self.postInstruction.text = post?.instructions
@@ -257,29 +269,22 @@ class PostDetailVC: BaseVC, UITableViewDelegate, UITableViewDataSource, deletePh
         cell.delegate = self
         return cell
     }
-    
-    @IBAction func dismissAction(sender: AnyObject){
-        if self.postInstruction.text.count == 0 {
-            self.showAlertMessage(title: "Alert", message: "You need to put in instructions for the influencer to follow.") {
-            }
-            return
-        }
-        for phase in self.phraseList {
-            if phase == "" {
-                self.showAlertMessage(title: "Alert", message: "You left a blank in your required caption items.") {
-                }
-                return
-            }
-            
-            if phase == "#" {
-                self.showAlertMessage(title: "Alert", message: "You left a blank hashtag in your required caption items.") {
-                }
-                return
-            }
-        }
+	
+	func tryDismiss() -> Bool {
+		if self.postInstruction.text.count == 0 {
+			self.navigationController?.popViewController(animated: true)
+			return true
+		}
+        self.phraseList = self.phraseList.filter{$0 != "" && $0.starts(with: "#") ? $0.count > 1 : $0.count > 0}
+		if self.phraseList.count == 0 {
+			self.showAlertMessage(title: "Post not complete", message: "Add at least one phrase in Caption Requirements to save."){ }
+			return false
+		}
+        
+ 
 		let tempHash = phraseList.filter { (hashtag) -> Bool in
-            return hashtag.starts(with: "#")
-        }
+			return hashtag.starts(with: "#")
+		}
 		var hash: [String] = []
 		for h in tempHash {
 			hash.append(String(h.dropFirst()))
@@ -287,22 +292,26 @@ class PostDetailVC: BaseVC, UITableViewDelegate, UITableViewDataSource, deletePh
 		if !hash.contains("ad") {
 			hash.append("ad")
 		}
-        let phase = phraseList.filter { (hashtag) -> Bool in
-            return !hashtag.starts(with: "#")
-        }
-        
-        if self.postIndex == nil{
-            let post = DraftPost.init(businessId: MyCompany.businessId, draftId: self.draftOffer!.draftId, poolId: "", hash: hash, keywords: phase, ins: self.postInstruction.text)
-            self.draftOffer?.draftPosts.append(post)
-        }else{
-            let modified = self.draftOffer?.draftPosts[self.postIndex!]
-            let postData = ["requiredHastags": hash, "requiredKeywords": phase, "instructions": self.postInstruction.text!] as [String: Any]
-            let post = DraftPost.init(dictionary: postData, businessId: MyCompany.businessId, draftId: self.draftOffer!.draftId, draftPostId: modified!.draftPostId, poolId: "")
-            self.draftOffer?.draftPosts[self.postIndex!] = post
-        }
-        
-        
-        self.navigationController?.popViewController(animated: true)
+		let phase = phraseList.filter { (hashtag) -> Bool in
+			return !hashtag.starts(with: "#")
+		}
+		
+		if self.postIndex == nil{
+			let post = DraftPost.init(businessId: MyCompany.businessId, draftId: self.draftOffer!.draftId, poolId: "", hash: hash, keywords: phase, ins: self.postInstruction.text)
+			self.draftOffer?.draftPosts.append(post)
+		}else{
+			let modified = self.draftOffer?.draftPosts[self.postIndex!]
+			let postData = ["requiredHastags": hash, "requiredKeywords": phase, "instructions": self.postInstruction.text!] as [String: Any]
+			let post = DraftPost.init(dictionary: postData, businessId: MyCompany.businessId, draftId: self.draftOffer!.draftId, draftPostId: modified!.draftPostId, poolId: "")
+			self.draftOffer?.draftPosts[self.postIndex!] = post
+		}
+		return true
+	}
+    
+    @IBAction func dismissAction(sender: AnyObject){
+		if tryDismiss() {
+			self.navigationController?.popViewController(animated: true)
+		}
     }
     
     @IBAction func addPhrase(_ sender: Any) {
